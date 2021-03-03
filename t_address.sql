68afa1aed9b02c004b2c72d26af3652bfb652728
mika <73530023+2everlove@users.noreply.github.com>
2021-03-03 13:38:03 +0900

    DB: JSP+DB

---
+++ t_address.sql
@@ -0,0 +1,17 @@
+-- creating table
+CREATE TABLE t_address (
+    num       NUMBER(4),
+    username  VARCHAR2(20) NOT NULL,
+    tel       VARCHAR2(20) NOT NULL,
+    email     VARCHAR2(50) NOT NULL,
+    gender    VARCHAR2(5),
+    joindate  DATE DEFAULT sysdate,
+    PRIMARY KEY ( num )
+);
+
+DESC t_address;
+
+-- 시퀀스(일련번호) 생성
+CREATE SEQUENCE abseq INCREMENT BY 1 START WITH 1 MINVALUE 1 MAXVALUE 1000 NOCYCLE NOCACHE;
+
+select * from t_address;
