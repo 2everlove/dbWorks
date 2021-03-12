77c051b1b75e6aa1ba64e967b93c19f348f7cc4c
mika <73530023+2everlove@users.noreply.github.com>
2021-03-12 15:11:51 +0900

    DB:sequence, create table t_board

--- jweb/members.sql
+++ jweb/members.sql
@@ -1,19 +1,35 @@
 --create t_member
-create table t_member(
-    memberId varchar2(20),
-    passwd varchar2(20) not null,
-    name varchar2(30) not null,
-    gender varchar2(10),
-    joinDate date default sysdate,
-    primary key(memberId)
+CREATE TABLE t_member(
+    memberid VARCHAR2(20),
+    passwd VARCHAR2(20) NOT NULL,
+    name VARCHAR2(30) NOT NULL,
+    gender VARCHAR2(10),
+    joindate DATE DEFAULT sysdate,
+    PRIMARY KEY(memberid)
 );
 
-create table t_board(
-    bnum number(4),
-    title varchar2(200),
-    content varchar2(2000),
-    regDate date DEFAULT sysdate,
-    primary key(bnum)
+-- t_board 게시판 테이블 생성
+CREATE TABLE t_board(
+    bnum NUMBER(4) primary key,
+    title VARCHAR2(100) NOT NULL,
+    content VARCHAR2(2000) NOT NULL,
+    regDate DATE DEFAULT sysdate,
+    memberId VARCHAR2(20),
+    CONSTRAINT FK_MemberBoard FOREIGN KEY(memberId) REFERENCES t_member(memberId)
 );
 
-select * from t_member;
+-- 시퀀스 생성 : 게시판 번호(bnum)
+CREATE SEQUENCE bSeq
+INCREMENT BY 1 
+START WITH 1
+MINVALUE 1
+MAXVALUE 1000 
+NOCYCLE
+NOCACHE;
+
+desc t_board;
+
+drop table t_board;
+
+select * from t_board;
+select* FROM t_member;
