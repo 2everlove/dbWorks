d00f7aaa4a073d8648c629fcfb099f686dc19d19
mika <73530023+2everlove@users.noreply.github.com>
2021-03-22 16:59:15 +0900

    DB: value change

--- jweb/members.sql
+++ jweb/members.sql
@@ -10,16 +10,25 @@ CREATE TABLE t_member(
 
 -- t_board 게시판 테이블 생성
 CREATE TABLE t_board(
-    bnum NUMBER(4) primary key,
+    bnum NUMBER(4) PRIMARY KEY,
     title VARCHAR2(100) NOT NULL,
     content VARCHAR2(2000) NOT NULL,
-    regDate DATE DEFAULT sysdate,
-    memberId VARCHAR2(20),
-    CONSTRAINT FK_MemberBoard FOREIGN KEY(memberId) REFERENCES t_member(memberId)
+    regdate DATE DEFAULT sysdate,
+    memberid VARCHAR2(20),
+    CONSTRAINT fk_memberboard FOREIGN KEY(memberid) REFERENCES t_member(memberid)
 );
 
+-- 외래키 제약 조건 삭제
+ALTER TABLE t_board DROP CONSTRAINT fk_memberboard;
+
+-- 외래키 제약 조건 : 연쇄적으로 삭제
+ALTER TABLE t_board ADD CONSTRAINT fk_memberboard FOREIGN KEY(memberid) REFERENCES t_member(memberid) ON DELETE CASCADE;
+
+--조회수 칼럼 추가
+ALTER TABLE t_board ADD (hit NUMBER(5));
+
 -- 시퀀스 생성 : 게시판 번호(bnum)
-CREATE SEQUENCE bSeq
+CREATE SEQUENCE bseq
 INCREMENT BY 1 
 START WITH 1
 MINVALUE 1
@@ -27,9 +36,16 @@ MAXVALUE 1000
 NOCYCLE
 NOCACHE;
 
-desc t_board;
+DROP SEQUENCE bseq;
 
-drop table t_board;
+DESC t_board;
 
-select * from t_board;
-select* FROM t_member;
+DROP TABLE t_board;
+
+DELETE FROM t_board WHERE bnum = 18;
+
+commit;
+
+SELECT * FROM t_board;
+SELECT * FROM t_board WHERE memberid='ddaddut' ORDER BY bnum DESC ;
+SELECT* FROM t_member;
